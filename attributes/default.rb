e9582c367b0af922f7f6d20105843a6c863b6fd4
Pedro <pedro.vilaca.dev@gmail.com>
2014-05-19 16:59:40 +0100

    Fixing default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['nginx']['reverseproxy']['server']  = '127.0.0.1'
-default['nginx']['reverseproxy']['port']    = '8080'
+default['nginx']['reverseproxy']['destination_ip']      = '127.0.0.1'
+default['nginx']['reverseproxy']['destination_port']    = '8080'
